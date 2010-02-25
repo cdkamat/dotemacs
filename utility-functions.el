e272c0f3b5638eea1a52148c0ca55f711e1b546b
Chinmay Kamat <chinmaykamat@gmail.com>
2010-02-24 21:37:39 -0700

    removed complete line from hippie-expand

--- utility-functions.el
+++ utility-functions.el
@@ -51,7 +51,6 @@ Symbols matching the text at point are put first in the completion list."
        '(try-expand-dabbrev
          try-expand-dabbrev-from-kill
          try-expand-dabbrev-all-buffers
-         try-expand-line
          try-complete-file-name-partially
          try-complete-file-name
          try-complete-lisp-symbol-partially
