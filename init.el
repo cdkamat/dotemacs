dca26eb1f68a3493bb1c10210d210b431cd2d779
Chinmay Kamat <chinmaykamat@gmail.com>
2010-08-01 21:42:55 +0530

    added ELPA

--- init.el
+++ init.el
@@ -112,3 +112,13 @@
   (defun server-ensure-safe-dir (dir) "Noop" t))
 
 (require 'c-eldoc)
+
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
