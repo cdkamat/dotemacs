cdkamat/dotemacs

commit 985fd4b5d2234673966e8dc4ea5bf696ef1a5053

changes to load color-theme-wombat after creation of frame

plugins/color-theme-6.6.0/themes/color-theme-wombat.el
@@ -57,7 +57,7 @@
 (defvar wombat-gray "#424242")
 (defvar wombat-gray+1 "#99968b")
 
-(defun color-theme-wombat ()
+(defun color-theme-wombat (&optional frame)
   "The wombat color theme for Emacs."
   (interactive)
   ;; set font 

init.el
@@ -97,6 +97,7 @@
 (require 'folio)
 (if window-system
     (color-theme-wombat))
+(add-hook 'after-make-frame-functions 'color-theme-wombat)
 
 ;; PKGBUILD mode
 (autoload 'pkgbuild-mode "pkgbuild-mode.el" "PKGBUILD mode." t)

commit 3dd5d6a962a66728a90d5bf9fd5f83b9666bb977

Fixed font selection. Was broken after 985fd4b5d2234673966e8dc4ea5bf696ef1a5053

plugins/color-theme-6.6.0/themes/color-theme-wombat.el
@@ -61,7 +61,7 @@
   "The wombat color theme for Emacs."
   (interactive)
   ;; set font 
-  (set-frame-font "Consolas-13")
+  (set-frame-parameter frame 'font "Consolas-13")
   (color-theme-install
    `(color-theme-wombat
      ((background-color . ,wombat-bg)