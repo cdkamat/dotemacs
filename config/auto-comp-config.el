3ccd47796aa37105dbe1f039e819c390e7017670
Chinmay Kamat <chinmaykamat@gmail.com>
2011-09-30 00:58:30 -0400

    Fixed some auto-complete settings.

--- config/auto-comp-config.el
+++ config/auto-comp-config.el
@@ -11,13 +11,13 @@
       ac-auto-show-menu 1
       ac-use-menu-map t)
 
-(dolist (mode '(org-mode text-mode))
+(dolist (mode '(org-mode text-mode asm-mode))
   (add-to-list 'ac-modes mode))
 
 ;; Key Bindings
 (ac-set-trigger-key "TAB")
 (define-key ac-menu-map "\C-n" 'ac-next)
 (define-key ac-menu-map "\C-p" 'ac-previous)
-(define-key ac-completing-map "\t" 'ac-complete)
+(define-key ac-completing-map [tab] 'ac-complete)
 
 (provide 'auto-comp-config)
