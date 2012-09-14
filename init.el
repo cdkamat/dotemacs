8561da3ef9a97b6d4611212e18e75d03ab945302
Chinmay Kamat <chinmaykamat@gmail.com>
2012-09-14 02:25:47 -0400

    Removed elpa. Will use emacs24's inbuilt package.el.

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 ;; init.el Loads everything
-;; Last modified : Sun, 23 October 2011 20:03:29 EDT
+;; Last modified : Fri, 14 September 2012 02:13:48 EDT
 
 ;;Emacs load path
 (let* ((my-lisp-dir "~/.emacs.d/")
@@ -21,12 +21,7 @@
 ;;Custom keybindings
 (require 'misc-bindings)
 
-;;; This was installed by package-install.el.
-;;; This provides support for the package system and
-;;; interfacing with ELPA, the package archive.
-;;; Move this code earlier if you want to reference
-;;; packages in your .emacs.
-(when
-    (load
-     (expand-file-name "~/.emacs.d/elpa/package.el"))
-  (package-initialize))
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
