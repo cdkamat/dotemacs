89c30f4755b71eec3579f047f2562499a87f6eb2
Chinmay Kamat <chinmaykamat@gmail.com>
2014-05-05 13:53:39 -0700

    Changed el-get install file to use cdkamat repo. Also changed default protocol for github to https.

--- config/cdk-config.el
+++ config/cdk-config.el
@@ -13,13 +13,14 @@
   (make-directory (concat cdk-lisp-dir "plugins/el-get") t))
 
 (setq el-get-dir (concat cdk-lisp-dir "plugins/el-get/"))
+(setq el-get-github-default-url-type 'https)
 
 ;; el-get bootstrap
 (unless (require 'el-get nil t)
   (setq el-get-install-branch "master")
   (with-current-buffer
       (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+       "https://raw.github.com/cdkamat/el-get/master/el-get-install.el")
     (end-of-buffer)
     (eval-print-last-sexp))
   (el-get-emacswiki-refresh el-get-recipe-path-emacswiki t))
